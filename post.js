c34d35fb3884ace4727a73333244587fbcb7b1e3
Jamilly Braga <114257647+jamilly-b@users.noreply.github.com>
2023-11-01 23:46:10 -0300

    Delete post.js

--- post.js
+++
@@ -1,9 +0,0 @@
-function newPost(){
-  let ul = document.getElementById("lista-posts");
-  let novoPost = document.createElement("li");
-  let content = document.getElementById("text-tweet").value; 
-  let paragrafo = document.createElement("p");
-  paragrafo.textContent = content;
-  novoPost.appendChild(paragrafo);
-  ul.appendChild(novoPost);
-}
